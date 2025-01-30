da17e046e6791e3edb2ff727cfec0fa94343caff
d4nitrix13 <danielperezdev@proton.me>
2025-01-30 13:16:16 -0600

    fichero index.php, punto de entrada para la aplicación web.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php require __DIR__ . "/partials/header.php" ?>
+
+<body>
+    <?php require __DIR__ . "/partials/navbar.php" ?>
+</body>
+<?php require __DIR__ . "/partials/footer.php" ?>
+
+</html>
