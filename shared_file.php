d76616a9ae410ac04208f8fd397b068e78e31d42
d4nitrix13 <danielperezdev@proton.me>
2025-01-30 13:16:17 -0600

    fichero shared_file.php que gestiona el proceso de compartición de ficheros.

---
+++ shared_file.php
@@ -0,0 +1,120 @@
+<?php
+session_start();
+$connection = require "./sql/db.php";
+require_once "./utils/functions.php";
+$appRootDirectory = realpath(dirname(__FILE__));
+
+$directoryAppIcon = $appRootDirectory . "/static/icon/";
+
+$statement = $connection->prepare(
+    "SELECT u.email, s.id_user_property AS id, s.path, f.name
+        FROM shared_file s
+        JOIN users u ON u.id = s.id_user_property
+        JOIN files f ON f.path = s.path 
+        WHERE s.id_user_guest = :id AND s.shared_from_a_directory = false"
+);
+$statement->execute([
+    ":id" => $_SESSION["user"]["id"]
+]);
+$listSharedFile = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+<?php require __DIR__ . "/partials/header.php" ?>
+
+<body>
+    <?php require __DIR__ . "/partials/navbar.php" ?>
+    <div class="container mb-3 mt-4 pt-2 d-grid gap-2">
+        <h2 class="justify-content-center align-items-center">Shared Files</h2>
+        <hr>
+        <div class="container mb-3 mt-4 pt-2 d-grid gap-2">
+            <!-- Files -->
+            <?php foreach ($listSharedFile as $key => $file): ?>
+                <?php
+                $statement = $connection->prepare(
+                    "SELECT name, type, path, size, file_creation_date FROM files WHERE id_user = :id AND name = :name LIMIT 1"
+                );
+
+                $statement->execute([
+                    ":id" => $file["id"],
+                    ":name" => $file["name"],
+                ]);
+                $data = $statement->fetch(PDO::FETCH_ASSOC);
+                ?>
+                <div class="container pt-4 p-3">
+                    <div class="row">
+                        <div class="accordion"
+                            id="accordionExample">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button d-flex align-items-center"
+                                        type="button"
+                                        data-bs-toggle="collapse"
+                                        data-bs-target="#collapse<?= $indice ?>"
+                                        aria-expanded="true"
+                                        aria-controls="collapse<?= $indice ?>">
+                                        <span class="me-auto">
+                                            <?= $file["name"] ?>
+                                        </span>
+                                        <label class="me-2">Propietario:
+                                            <?= $file["email"] ?>
+                                        </label>
+
+                                        <?php $icon = getFileExtension(dirname(realpath($file["path"])), $file["name"]); ?>
+                                        <img style="width: 2.0rem;"
+                                            class="ms-auto"
+                                            src="./static/icon/<?= detectExtension($icon); ?>.png"
+                                            alt="Icon">
+                                    </button>
+                                </h2>
+                                <div id="collapse<?= $indice ?>"
+                                    class="accordion-collapse collapse show"
+                                    data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <div class="card-body">
+                                            <h5 class="card-title"></h5>
+                                        </div>
+                                        <ul class="list-group list-group-flush">
+                                            <li class="list-group-item m-1">Type:
+                                                <?= $data["type"] ?>
+                                            </li>
+                                            <li class="list-group-item m-1">Size:
+                                                <?= $data["size"] ?> bytes
+                                            </li>
+                                            <li class="list-group-item m-1">Fecha Creacion Del Fichero:
+                                                <?= date("Y-m-d H:i:s", strtotime($data["file_creation_date"])) ?>
+                                            </li>
+                                        </ul>
+                                        <div class="card-body d-flex gap-3">
+                                            <?php $params = http_build_query([
+                                                "idProperty" => $file["id"],
+                                                "idGuest" => $_SESSION['user']['id'],
+                                                "sharedFilePath" => $data["path"]
+                                            ]); ?>
+                                            <a href="./download_shared_file.php?<?= $params ?>"
+                                                class="card-link">Descargar</a>
+
+                                            <?php require_once "./utils/list.php";
+                                            if (in_array($data["type"], $listContentType)): ?>
+                                                <?php $params = http_build_query([
+                                                    "idProperty" => $file["id"],
+                                                    "idGuest" => $_SESSION['user']['id'],
+                                                    "sharedFilePath" => $data["path"]
+                                                ]); ?>
+                                                <a href="serve_shared_file.php?<?= $params ?>"
+                                                    class="card-link">Ver Contenido</a>
+                                            <?php endif ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</body>
+<?php require __DIR__ . "/partials/footer.php" ?>
+
+</html>
